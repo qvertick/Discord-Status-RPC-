9642dff2486185b03582d731b0aed7be326b73ac
qvertick <pbezuglov98@gmail.com>
2026-07-19 13:25:57 +0200

    Edit bugs

--- DS_RPC.py
+++ DS_RPC.py
@@ -2,7 +2,7 @@ import time
 from pypresence import Presence
 
 # Сюда вставляешь свой скопированный Application ID
-client_id = "1509256034169126992"
+client_id = ""
 
 # Подключаемся к Дискорду
 RPC = Presence(client_id)
@@ -12,8 +12,8 @@ print("Статус активен, троляканье = true")
 
 # Настройка того, что будет написано в статусе
 RPC.update(
-    state="tg : @qvertick",        # Нижняя строчка текста
-    details="Альтухи Краков ГАЗ знакомиться",            # Верхняя строчка текста
+    state="tg : @XXX",        # Нижняя строчка текста
+    details="XXX",            # Верхняя строчка текста
     start=time.time(),                  # Включает счетчик времени
     large_image="logo",                 # Имя картинки из Art Assets
     large_text="<3",       # Текст при наведении на картинку
